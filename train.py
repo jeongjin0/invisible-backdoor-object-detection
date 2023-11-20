81e06a5a9e39e1c50362e10745a19553193dfe7b
jeongjin-shin <80797980+jeongjin-shin@users.noreply.github.com>
2023-11-20 14:01:26 +0900

    Update train.py

--- train.py
+++ train.py
@@ -257,8 +257,7 @@ def train(**kwargs):
         best_path = trainer.save(best_asr=filename)
         best_path2 = atk_model.save(best_asr=filename)
         if epoch == 9:
-            #lr_ = lr_ * opt.lr_decay
-            pass
+            lr_ = lr_ * opt.lr_decay
 
         if epoch == 13: 
             break
